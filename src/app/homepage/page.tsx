803b5cc4493c2216a25574f26464383e429789d0
Abdul Wahab Asif <abdulwahab5547@gmail.com>
2024-10-18 17:39:02 +0500

    Your commit message

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,9 +3,12 @@ import FocusTask from "../components/focustask";
 
 function Homepage(){
     return(
-        <div className="flex flex-col justify-center items-center min-h-screen gap-7 py-16">
+        <div className="flex flex-col justify-center items-center min-h-screen gap-7 py-16 relative">
             <Pomodoro/>
             <FocusTask/>
+            <div className="absolute bottom-5">
+                <p className="text-sm md:text-base">App by <a className='underline' href="http://abdulwahabasif.com/">Abdul</a></p>
+            </div>
         </div>
     )
 }
